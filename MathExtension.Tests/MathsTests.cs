f6438c4172cdd9a52a66e9cf8e33a1e63f1d0e50
Lenovo <Lenovo@Y50>
2017-07-06 00:28:49 +0300

    Add project files.

---
+++ MathExtension.Tests/MathsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExtension.Tests
+{
+    public class MathsTests
+    {
+        [TestCase(2, 2, 0.00001)]
+        [TestCase(2, 3, 0.00001)]
+        [TestCase(2.555, 7, 0.00001)]
+        [TestCase(0.0001, 3, 0.00001)]
+        public void Sqrt_PositiveTest(double a, int n, double eps)
+        {
+            Assert.IsTrue(Math.Abs(MathExtension.Maths.Sqrt(a, n, eps) - Math.Pow(a, 1.0/n)) < eps);
+        }
+
+        [TestCase(8, 15, -1)]
+        [TestCase(8, 0, 1)]
+        [TestCase(-8, 15, 0.1)]
+        public void Sqrt_ThrowsArgumentOutOfRangeException(double a, int n, double eps)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => MathExtension.Maths.Sqrt(a, n, eps));
+        }
+    }
+}
---
+++ MathExtension/Maths.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathExtension
+{
+    public static class Maths
+    {
+        /// <summary>
+        /// A method to get nth root of a positive real number a. Using Newton's method.
+        /// </summary>
+        /// <param name="a">A positive real number to get nth root</param>
+        /// <param name="n">deegree of a root</param>
+        /// <param name="eps">precision</param>
+        /// <returns> nth root of a positive real number a</returns>
+        public static double Sqrt(double a, int n, double eps = 0.000001)
+        {
+            CheckoutException(a, n, eps);
+            double x0 = a / n;
+            double x1 = CalculateNext(a, n, x0);
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = CalculateNext(a, n, x0);
+            }
+
+            return x1;
+        }
+
+        private static void CheckoutException(double a, int n, double eps)
+        {
+            if (a <= 0) throw new ArgumentOutOfRangeException($"{nameof(a)} must be positive");
+            if (n < 1) throw new ArgumentOutOfRangeException($"{nameof(a)} must greater than 0");
+            if (eps < 0) throw new ArgumentOutOfRangeException($"{nameof(a)} must be positive");
+        }
+
+        private static double CalculateNext(double a, int n, double x0)
+        {
+            return (1.0 / n) * ((n - 1) * x0 + a / Math.Pow(x0, n - 1));
+        }
+    }
+}
